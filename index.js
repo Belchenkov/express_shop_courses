866085335aea862e2795275d557f23decd8080d6
Aleksey Belchenkov <u608110@gmail.com>
2019-07-20 17:15:48 +0300

    Include Handlebars

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const path = require('path');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
+// Handlebars
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+// Register `hbs.engine` with the Express app.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+    res.render('about');
 });
 
 const PORT = process.env.PORT || 3000;
