77c8c7a82c53646ae93083eab71d52b76680cd64
Aleksey Belchenkov <u608110@gmail.com>
2019-07-25 06:56:20 +0300

    Create Model Course

--- routes/add.js
+++ routes/add.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 
+const Course = require('../models/course');
+
 router.get('/',  (req, res) => {
     res.render('add', {
         title: 'Добавить курс',
@@ -8,8 +10,12 @@ router.get('/',  (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post('/', async (req, res) => {
+    const course = new Course(req.body.title, req.body.price, req.body.img);
+
+    await course.save();
+
+    res.redirect('/courses');
 });
 
 module.exports = router;
