c030ecaca32aadb69848550a1bb2519908316fbd
Aleksey Belchenkov <u608110@gmail.com>
2019-07-30 09:05:07 +0300

    Edit course

--- routes/courses.js
+++ routes/courses.js
@@ -18,9 +18,28 @@ router.get('/:id', async (req, res) => {
 
     res.render('course', {
         layout: 'empty',
-        title: `Курс - ${course.title}`,
+        title: `Курс`,
         course
     });
+});
+
+router.get('/:id/edit', async (req, res) => {
+    if (!req.query.allow) {
+        return res.redirect('/');
+    }
+
+    const course = await Course.getById(req.params.id);
+
+    res.render('course-edit', {
+       title: `Редактировать ${course.title}`,
+       course
+    });
+});
+
+router.post('/edit', async (req, res) => {
+    await Course.update(req.body);
+
+    res.redirect('/courses');
 });
 
 module.exports = router;
