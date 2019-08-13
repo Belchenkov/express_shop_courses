3ee9ce81b89f425403ed72c2b37184dff212b435
Aleksey Belchenkov <u608110@gmail.com>
2019-08-13 07:11:24 +0300

    Setting SendGrid Service

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGODB_URI: 'mongodb://belchenkov:12qwasZX@ds013559.mlab.com:13559/express_shop_courses',
-    SESSION_SECRET: 'some secret value'
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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'
 };
