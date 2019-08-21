05817a218d381f8053d6a5da17fe50b1ba09a8c3
Aleksey Belchenkov <u608110@gmail.com>
2019-08-21 07:33:29 +0300

    Validate Form Add/Edit

--- routes/add.js
+++ routes/add.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const router = Router();
+const { validationResult } = require('express-validator');
 
 const auth = require('../middleware/auth');
 const Course = require('../models/course');
+const { courseValidators } = require('../utils/validators');
 
 router.get('/', auth, (req, res) => {
     res.render('add', {
@@ -11,7 +13,22 @@ router.get('/', auth, (req, res) => {
     });
 });
 
-router.post('/', auth, async (req, res) => {
+router.post('/', auth, courseValidators, async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('add', {
+            title: 'Добавить курс',
+            isAdd: true,
+            error: errors.array()[0].msg,
+            data: {
+                title: req.body.title,
+                price: req.body.price,
+                img: req.body.img
+            }
+        });
+    }
+
     const course = new Course({
         title: req.body.title,
         price: req.body.price,
