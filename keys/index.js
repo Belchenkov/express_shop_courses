ee3e1fca346869f600e3a8cb44f1ea93e20f57dd
Aleksey Belchenkov <u608110@gmail.com>
2019-08-25 07:57:45 +0300

    Protected keys

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGODB_URI: 'mongodb://belchenkov:12qwasZX@ds013559.mlab.com:13559/express_shop_courses',
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'nodejs-courses@gmail.ru',
-    BASE_URL: 'http://localhost:3000'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod');
+} else {
+    module.exports = require('./keys.dev');
+}
---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGODB_URI: 'mongodb://belchenkov:12qwasZX@ds013559.mlab.com:13559/express_shop_courses',
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'nodejs-courses@gmail.ru',
+    BASE_URL: 'http://localhost:3000'
+};
